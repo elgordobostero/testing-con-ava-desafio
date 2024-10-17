f62d55985272160fec7282b93cce8f51dbce8e40
Valentin Goroso <valegoroso21@gmail.com>
2024-10-17 18:07:48 -0300

    Complete el test.ts, funciona correctamente

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,18 @@
 import test from "ava";
-import { Product } from "./index";
+import { Product, User } from "./index";
 
 test("Testeo la clase Product", (t) => {
   // test Product class
-  const product = new Product("Product 1", 10);
-  t.is(product.name, "Product 1");
-  t.is(product.price, 10);
+  const product = new Product("Termo Stanley", 60000);
+  t.is(product.name, "Termo Stanley");
+  t.is(product.price, 60000);
 });
-test("Testeo la clase User y sus métodos", (t) => {});
+test("Testeo la clase User y sus métodos", (t) => {
+  //   mock
+  const usuario = new User("Valentin");
+  const product1 = new Product("Termo Lumilagro", 45000);
+  usuario.addProduct(product1);
+  t.is(usuario.name, "Valentin");
+  t.is(usuario.products[0].name, "Termo Lumilagro");
+  t.is(usuario.products[0].price, 45000);
+});
